2190b57c63491b8749d6aefbceebdbb93dc72819
zgscsed <1106659535@qq.com>
2022-03-16 23:45:37 +0800

    创建项目

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2022 zgscsed. All rights reserved.
+ * @filename: main.cpp
+ * @Author: zgscsed
+ * @Date: 2022-03-16 23:43:25
+ * @LastEditors: zgscsed
+ * @LastEditTime: 2022-03-16 23:44:35
+ * @Description: main函数。
+ */
+
+#include <iostream>
+
+int main(void)
+{
+
+    std::cout << "HiNetServer..."<<std::endl;
+    return 0;
+}
